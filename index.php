a765e7cf0637482742911a2240c4a87d1a087ea9
Rhett Amin <rhettamin@gmail.com>
2015-01-13 17:06:30 -0500

    repload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang = "en">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible"  content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel='stylesheet' type='text/css' href='css/bootstrap.css'> 	
+		<title>FISHMO</title>
+		
+	</head>
+
+	<body class="background">
+		<div class="container-login">
+		
+			<div class="col-md-6 col-md-offset-3-5">
+				<h1>Welcome to the Fishmo Database</h1>
+
+				<div class = "col-md-6 col-md-offset-2" >
+
+					<div class="panel panel-default">
+					  <div class="panel-heading"><strong>Login</strong></div>
+					  <div class="panel-body">
+					    <input type="text" name="username" placeholder="Username"/><br><br>
+						<input type="password" name="password" placeholder="Password"/><br><br>
+
+						<button type="button">Login</button>
+						<button type="button">Register</button>
+					  </div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
